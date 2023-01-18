70383bb37e79001d7d5514ce2a3072f14ab84a35
nicolae-co <cojocarugithub@gmail.com>
2023-01-18 11:57:35 +0200

    map

--- AirBnbClone/src/App.jsx
+++ AirBnbClone/src/App.jsx
@@ -2,24 +2,30 @@ import './App.css'
 import Card from './components/Card'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
-
-import katieImg from "./assets/katie-zaferes.png"
+import data from './data/data.js'
 
 
 function App() {
+  const dataMap = data.map(item => {
+    return(
+      <Card
+        key={item.id}
+      img={`./images/${item.coverImg}`}
+      rating={item.stats.rating}
+      reviewCount={item.stats.reviewCount}
+      country={item.location}
+      title={item.title}
+      price={item.price}
+    />
+  )})
 
   return (
     <div className="App">
       <Navbar />
       {/* <Hero /> */}
-      <Card
-        img={katieImg}
-        rating="5.0"
-        reviewCount={6}
-        country="USA"
-        title="Life Lessons with Katie Zaferes"
-        price={136}
-      />
+      <section className='card-list'>
+        {dataMap}
+      </section>
     </div>
   )
 }
